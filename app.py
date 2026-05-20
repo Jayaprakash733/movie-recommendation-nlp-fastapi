916d6193b9a7574d677e1a4e792f85045fadd638
Jayaprakash <mrjpjayapraksh@gmail.com>
2026-05-21 00:53:21 +0530

    Updated deployed backend URL

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 # CONFIG
 # =============================
 
-API_BASE = "https://movie-rec-466x.onrender.com" 
+API_BASE = "https://movie-api-jsw7.onrender.com"
 TMDB_IMG = "https://image.tmdb.org/t/p/w500"
 
 st.set_page_config(page_title="Movie Recommender", page_icon="🎬", layout="wide")
